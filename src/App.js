7c5f981ff225cec638b0c63f664c830e0f71c56c
pavithrank02 <pavithrank02@gmail.com>
2023-11-07 18:58:44 +0530

    transition positions

--- src/App.js
+++ src/App.js
@@ -15,19 +15,16 @@ function App() {
   };
 
   const animationName = isMovingRight ? 'moveRight' : 'moveLeft';
+
   return (
     <div className="App">
-      <div className="row">
-        <div className="image-row">
+      <div className={`image-container ${animationName}`}>
           <img src={img1} alt="Image 1" className={`moving-image ${animationName}`} />
           <img src={img2} alt="Image 2" className={`moving-image ${animationName}`} />
           <img src={img3} alt="Image 3" className={`moving-image ${animationName}`} />
-        </div>
-        <div className="image-row">
           <img src={img4} alt="Image 4" className={`moving-image ${animationName}`} />
           <img src={img5} alt="Image 5" className={`moving-image ${animationName}`} />
           <img src={img6} alt="Image 6" className={`moving-image ${animationName}`} />
-        </div>
       </div>
       <button onClick={startAnimation}>Toggle Direction</button>
     </div>
