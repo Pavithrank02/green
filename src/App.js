4b687ff4c5c94bf5e86e27c48eaf757dc6dde2f3
pavithrank02 <pavithrank02@gmail.com>
2023-11-07 17:52:49 +0530

    images placement

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import img1 from '../src/images/1.jpg'
+import img2 from '../src/images/2.jpg'
+import img3 from '../src/images/3.jpg'
+import img4 from '../src/images/4.jpg'
+import img5 from '../src/images/5.jpg'
+import img6 from '../src/images/6.jpg'
 import './App.css';
 
 function App() {
+  const [isMovingRight, setIsMovingRight] = useState(false);
+
+  const startAnimation = () => {
+    setIsMovingRight(!isMovingRight);
+  };
+
+  const animationName = isMovingRight ? 'moveRight' : 'moveLeft';
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="row">
+        <div className="image-row">
+          <img src={img1} alt="Image 1" className={`moving-image ${animationName}`} />
+          <img src={img2} alt="Image 2" className={`moving-image ${animationName}`} />
+          <img src={img3} alt="Image 3" className={`moving-image ${animationName}`} />
+        </div>
+        <div className="image-row">
+          <img src={img4} alt="Image 4" className={`moving-image ${animationName}`} />
+          <img src={img5} alt="Image 5" className={`moving-image ${animationName}`} />
+          <img src={img6} alt="Image 6" className={`moving-image ${animationName}`} />
+        </div>
+      </div>
+      <button onClick={startAnimation}>Toggle Direction</button>
     </div>
   );
 }
